d26431dcd8ccabd02fb059c71e5a23774ca8b363
Floyd E Moore <floyd.moore@hp.com>
2023-10-04 11:30:44 -0700

    Adding dashboard button to reset encoders

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,7 +6,9 @@ package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.ResetEncoders;
 import frc.robot.subsystems.Swerve;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -48,6 +50,7 @@ public class RobotContainer {
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
     m_driverController.b().whileTrue(m_swerveModule.reset_encoders());
+    SmartDashboard.putData("Reset Encoders", new ResetEncoders());
   }
 
   /**
